0bdc6990f2696dba95ec66cd05d5f1b6efba8aad
Rakesh Adhithya <rakkeshadithya00@gmail.com>
2025-08-19 16:38:41 +0530

    made query readable for 15 days of learning sql

--- 5. advance_join/15_days_of_learning_sql.sql
+++ 5. advance_join/15_days_of_learning_sql.sql
@@ -3,27 +3,26 @@ TOPIC: CO-RELATED SCALER SUBQUERIES, JOINS, DATEDIFF FUNCTION
 LINK: https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem?isFullScreen=true
 */
 
+-- tables: Submissions(sd, sid, hid, scr), Hackers(hid, nm)
+-- question: for each distinct sd, find all hackers who submitted till sd, find max hacker who submitted on sd if tie lowest id. sort results with sd asc
 
--- tables: submissions, hackers
--- quesion: for each unique submission date, find all hackers who submitted every day from 2016-03-01 till that submission date, 
--- find hacker who submitted max on that day if tie lowest id
-
+-- select sd, uc, id, name from
 SELECT sd, unique_count, h.hacker_id, h.name
 FROM(
-    -- distinct submission date
+    -- select sd
     SELECT sd,
-    -- all hackers who submitted all days till above date
+    -- find count of all hackers who sbmitted all days till sd
     (SELECT COUNT(DISTINCT s.hacker_id)
-    FROM Submissions s 
-    WHERE s.submission_date <= sd AND
-        -- count distinct submissions till above date of outer hacker
-        (SELECT COUNT(DISTINCT ss.submission_date)
-         FROM Submissions ss 
-         WHERE ss.hacker_id = s.hacker_id AND
-               ss.submission_date <= sd
-        ) = DATEDIFF(sd, '2016-03-01') + 1
+     FROM Submissions s
+     WHERE s.submission_date <= sd AND
+            -- count total distinct days s.hacker_id has submitted
+           (SELECT COUNT(DISTINCT ss.submission_date)
+            FROM Submissions ss
+            WHERE ss.submission_date <= sd AND
+                  ss.hacker_id = s.hacker_id
+           ) = DATEDIFF(sd, '2016-03-01') + 1
     ) AS unique_count,
-    -- max hacker id who submitted max on this date
+    -- find hacker who submitted max on sd
     (SELECT hacker_id
     FROM Submissions
     WHERE submission_date = sd
@@ -31,8 +30,10 @@ FROM(
     ORDER BY COUNT(submission_id) DESC, hacker_id
     LIMIT 1) AS max_hacker_id
     
+    -- from distinct dates
     FROM (SELECT DISTINCT submission_date AS sd
          FROM Submissions) AS unique_dates
 ) AS final
-JOIN Hackers h ON final.max_hacker_id = h.hacker_id
+JOIN Hackers h on final.max_hacker_id = h.hacker_id
 ORDER BY sd
+  
